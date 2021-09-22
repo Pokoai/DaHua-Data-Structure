049c0fa1d16f19266cc70b09e6ba2c34891a4f88
adair <huminme@outlook.com>
2021-09-22 21:48:10 +0800

    栈-链式结构

---
+++ DaHua/Chapter4_Stack/linkstack.c
@@ -0,0 +1,114 @@
+/*
+ * @Description: 《大话数据结构》栈-链式实现
+ * @Author: Adair Hu
+ * @WebSite: https://arctee.cn
+ * @Github: https://github.com/adairhu
+ * @Date: 2021-09-22 10:46:04
+ * @LastEditTime: 2021-09-22 18:39:57
+ * @FilePath: \DaHua\Chapter4_Stack\linkstack.c
+ * 『戒急戒躁，心装大盘。日日耕耘，精进成长。』
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "linkstack.h"
+
+
+//初
+void InitStack(LinkStack * s)
+{
+    s->top = NULL;
+    s->count = 0;
+}
+
+//空
+bool StackIsEmpty(LinkStack s)
+{
+    if (s.count == 0)
+        return true;
+    else 
+        return false;
+}
+
+//个
+int StackLength(LinkStack s)
+{
+    return s.count;
+}
+
+//增
+bool Push(LinkStack * s, ElemType e)
+{
+    LinkStackPtr pnode;
+
+    //分配内存空间
+    pnode = (LinkStackPtr)malloc(sizeof(StackNode));
+    if(pnode == NULL) {
+        puts("内存分配失败");
+        return false;
+    }
+    pnode->data = e;
+    pnode->next = s->top;
+    s->top = pnode;
+    s->count++;
+
+    return true;
+}
+
+//删
+bool Pop(LinkStack * s, ElemType * e)
+{
+    LinkStackPtr p = s->top; //s->top 是指向链栈节点的指针
+
+    //空异常处理
+    if (StackIsEmpty(*s)) {
+        puts("栈为空");
+        return false;
+    }
+     
+    *e = p->data;
+    s->top = p->next;
+    s->count--;
+
+    free(p);
+    return true;
+}
+
+//取
+bool GetTop(LinkStack s, ElemType *e)
+{
+    if (StackIsEmpty(s)) {
+        puts("栈为空.");
+        return false;
+    }
+
+    *e = s.top->data;
+
+    return true;
+}
+
+//遍
+void StackTraverse(LinkStack s, void (*func)(ElemType))
+{
+    LinkStackPtr p = s.top;
+
+    while (p) {
+        (*func)(p->data);
+        p = p->next;
+    }
+}
+
+//清
+void ClearStack(LinkStack * s)
+{
+    LinkStackPtr p = s->top;
+    LinkStackPtr q;
+
+    while (p) {
+        q = p;
+        p = p->next;
+        free(q);
+    }
+
+    s->count = 0; //一开始忘记了这句
+}
---
+++ DaHua/Chapter4_Stack/linkstack.h
@@ -0,0 +1,57 @@
+/*
+ * @Description: 《大话数据结构》栈-链式实现
+ * @Author: Adair Hu
+ * @WebSite: https://arctee.cn
+ * @Github: https://github.com/adairhu
+ * @Date: 2021-09-22 10:46:41
+ * @LastEditTime: 2021-09-22 21:31:47
+ * @FilePath: \DaHua\Chapter4_Stack\linkstack.h
+ * 『戒急戒躁，心装大盘。日日耕耘，精进成长。』
+ */
+
+#ifndef _LINKSTACK_H_
+#define _LINKSTACK_H_
+
+#include <stdbool.h>
+
+typedef int ElemType;
+//链栈结构
+typedef struct stackNode{
+    ElemType data;
+    struct stackNode * next;
+}StackNode;
+
+typedef StackNode * LinkStackPtr; //指向栈节点的指针
+
+typedef struct {
+    LinkStackPtr top; //栈的头指针，即链表的头指针
+    int count;
+}LinkStack; //至此链栈结构搭建完毕
+
+
+//初
+void InitStack(LinkStack * s);
+
+//空
+bool StackIsEmpty(LinkStack s);
+
+//个
+int StackLength(LinkStack s);
+
+//增
+bool Push(LinkStack * s, ElemType e);
+
+//删
+bool Pop(LinkStack * s, ElemType * e);
+
+//取
+bool GetTop(LinkStack s, ElemType *e);
+
+//遍
+void StackTraverse(LinkStack s, void (*func)(ElemType));
+
+//清
+void ClearStack(LinkStack * s);
+
+
+#endif
