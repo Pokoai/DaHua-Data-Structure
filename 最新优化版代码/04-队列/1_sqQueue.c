ec8a2c2ceb0d517c1dcdd121fad5cae3e26fbbc2
Pokoai <pokeusr@outlook.com>
2022-09-08 16:29:43 +0800

    变量名由 stack 改为 queue

--- 最新优化版代码/04-队列/1_sqQueue.c
+++ 最新优化版代码/04-队列/1_sqQueue.c
@@ -1,21 +1,21 @@
 /*
  * 功能: 循环队列
  * 作者: Guyue
- * 微信公众号: https://img.arctee.cn/one/pokeai-wechat.png
- * 网站：https://pokeai.cn
- * Github: https://github.com/Pokoai/DaHua-Data-Structure/tree/main/%E6%9C%80%E6%96%B0%E4%BC%98%E5%8C%96%E7%89%88%E4%BB%A3%E7%A0%81
+ * 微信公众号: httpQ://img.arctee.cn/one/pokeai-wechat.png
+ * 网站：httpQ://pokeai.cn
+ * Github: httpQ://github.com/Pokoai/DaHua-Data-Structure/tree/main/%E6%9C%80%E6%96%B0%E4%BC%98%E5%8C%96%E7%89%88%E4%BB%A3%E7%A0%81
  * Date: 2022-08-26
  */
 
 
 /*
  * 1. 为什么要用循环队列？ 
- * 如果用链表实现队列的话，很简单，只要确定一个头指针和一个尾指针即可。
- * 但这里我们选择用数组来实现，数组占用一块连续的内存地址，那么就要考虑内存的问题了，入队：尾指针递增，出队：头指针递增，
+ * 如果用**链表**实现队列的话，很简单，只要确定一个头指针和一个尾指针即可。
+ * 但这里我们选择用**数组**来实现，数组占用一块连续的内存地址，那么就要考虑内存的问题了，入队：尾指针递增，出队：头指针递增，
  * 可以看出不管是增加元素还是删除元素，指针所指的内存地址都在递增，那么前面的地址就浪费了，内存空间一直在变小。
  * 为了解决这个问题，循环队列应运而生。
  * 
- * ![](https://img.arctee.cn/one/202208271516620.png)
+ * ![](httpQ://img.arctee.cn/one/202208271516620.png)
  * 
  * 2. 实现循环队列的关键点：
  *  - 头指针 front: 指向第一个有效节点，尾指针 rear: 指向最后一个有效节点的后面一个节点
@@ -38,24 +38,24 @@
 
 typedef int ElemType;
 
-typedef struct stack {
+typedef struct queue {
     ElemType * pBase;  // 数组（用指针表示）
     int front;
     int rear;
-} STACK, *PSTACK;
+} QUEUE, *PQUEUE;
 
 
-void initQueue(PSTACK pS);
-bool isFull(PSTACK pS);
-bool enQueue(PSTACK pS, ElemType e);
-bool isEmpty(PSTACK pS);
-bool traverse(PSTACK pS);
-bool deQueue(PSTACK pS, ElemType * pE);
+void initQueue(PQUEUE pQ);
+bool isFull(PQUEUE pQ);
+bool enQueue(PQUEUE pQ, ElemType e);
+bool isEmpty(PQUEUE pQ);
+bool traverse(PQUEUE pQ);
+bool deQueue(PQUEUE pQ, ElemType * pE);
 
 
 int main(void)
 {
-    STACK S;
+    QUEUE S;
 
     initQueue(&S);
 
@@ -85,51 +85,51 @@ int main(void)
 }
 
 // 初始化
-void initQueue(PSTACK pS)
+void initQueue(PQUEUE pQ)
 {
     // 创建动态数组
-    pS->pBase = (ElemType*)malloc( MAXSIZE * sizeof(ElemType) );
-    pS->front = 0;
-    pS->rear = 0;
+    pQ->pBase = (ElemType*)malloc( MAXSIZE * sizeof(ElemType) );
+    pQ->front = 0;
+    pQ->rear = 0;
 }
 
 // 是否已满
-bool isFull(PSTACK pS)
+bool isFull(PQUEUE pQ)
 {
-    return ( (pS->rear+1) % MAXSIZE ==  pS->front );
+    return ( (pQ->rear+1) % MAXSIZE ==  pQ->front );
 }
 
 // 入队
-bool enQueue(PSTACK pS, ElemType e)
+bool enQueue(PQUEUE pQ, ElemType e)
 {
-    if ( isFull(pS) ) {
+    if ( isFull(pQ) ) {
         printf("队列已满！无法入队！");
         return false;
     }
 
-    pS->pBase[pS->rear] = e;
-    pS->rear = (pS->rear + 1) % MAXSIZE;
+    pQ->pBase[pQ->rear] = e;
+    pQ->rear = (pQ->rear + 1) % MAXSIZE;
 
     return true;
 }
 
 // 是否空
-bool isEmpty(PSTACK pS)
+bool isEmpty(PQUEUE pQ)
 {
-    return ( pS->front == pS->rear );
+    return ( pQ->front == pQ->rear );
 }
 
 // 遍历队列
-bool traverse(PSTACK pS)
+bool traverse(PQUEUE pQ)
 {
-    if ( isEmpty(pS) ) {
+    if ( isEmpty(pQ) ) {
         printf("队列为空！");
         return false;
     }
 
-    int i = pS->front;
-    while ( i != pS->rear ) {
-        printf("%d ", pS->pBase[i]);
+    int i = pQ->front;
+    while ( i != pQ->rear ) {
+        printf("%d ", pQ->pBase[i]);
         i = (i + 1) % MAXSIZE;
     }
     printf("\n");
@@ -138,15 +138,15 @@ bool traverse(PSTACK pS)
 }
 
 // 出队
-bool deQueue(PSTACK pS, ElemType * pE)
+bool deQueue(PQUEUE pQ, ElemType * pE)
 {
-    if ( isEmpty(pS) ) {
+    if ( isEmpty(pQ) ) {
         printf("队列为空！");
         return false;
     }
 
-    *pE = pS->pBase[pS->front];
-    pS->front = (pS->front + 1) % MAXSIZE;
+    *pE = pQ->pBase[pQ->front];
+    pQ->front = (pQ->front + 1) % MAXSIZE;
 
     return true;
 }
